076f03cbcfccd774191a2f1719276f6a00eb1148
SrLozano <100383511@alumnos.uc3m.es>
2021-03-25 23:34:26 +0100

    Header implemented and new component ⚙️

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { MenuComponent } from './menu/menu.component';
 import { StretchingsComponent } from './stretchings/stretchings.component';
 
 const routes: Routes = [  
   { path: 'welcome', component: WelcomeComponent },
+  { path: 'menu', component: MenuComponent },
   { path: 'stretchings', component: StretchingsComponent },
   { path:'**', pathMatch: 'full', redirectTo: 'welcome'}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatButtonModule} from '@angular/material/button';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     StretchingsComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    MenuComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
